62dddd6847f7f01102fef3ea602ffa569c7fa7c8
angrycola <youagain@yandex.com>
2018-01-25 11:33:21 +0300

    Filter List

--- src/containers/ListCharacters/index.js
+++ src/containers/ListCharacters/index.js
@@ -1,6 +1,14 @@
 import { connect } from 'react-redux';
 import ListCharacters from '../../components/ListCharacters';
 
-const mapStateToProps = ({ characters }) => ({ characters });
+
+const mapStateToProps = ({ characters, filters }) => {
+	return ({ characters: characters.filter(character =>
+		(filters.name === '' || character.name === filters.name) &&
+		(filters.causeOfDeath === '' || character.causeOfDeath === filters.causeOfDeath) &&
+		(filters.takenBy === '' || character.takenBy === filters.takenBy) &&
+		(filters.killedBy === '' || character.killedBy === filters.killedBy)
+	)});
+};
 
 export default connect(mapStateToProps)(ListCharacters);
