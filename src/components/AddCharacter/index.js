005eb2012cffd337d5497fbb33538b6f2e18ac24
angrycola <youagain@yandex.com>
2018-01-27 13:27:42 +0300

    Handle form errors

--- src/components/AddCharacter/index.js
+++ src/components/AddCharacter/index.js
@@ -7,27 +7,47 @@ export default class AddCharacter extends Component {
 		description: '',
 		killedBy: '',
 		name: '',
-		takenBy: ''
+		takenBy: '',
+		errors: {}
 	}
 
 	handleSubmit = event => {
 		event.preventDefault();
 		const { causeOfDeath, description, killedBy, name, takenBy } = this.state;
-		if (causeOfDeath !== '' && description !== '' && killedBy !== '' && name !== '' && takenBy !== '') {
+		let errors = {};
+		const message = 'this field is required';
+
+		if (causeOfDeath === '') errors.causeOfDeath = message;
+		if (description === '') errors.description = message;
+		if (killedBy === '') errors.killedBy = message;
+		if (name === '') errors.name = message;
+		if (takenBy === '') errors.takenBy = message;
+
+		this.setState({ errors });
+
+		const isValid = Object.keys(errors).length === 0;
+		if (isValid) {
 			this.props.addCharacter({ causeOfDeath, description, killedBy, name, takenBy });
+			this.setState({causeOfDeath: '',	description: '', killedBy: '',	name: '',	takenBy: ''})
 		}
-		this.setState({	causeOfDeath: '',	description: '', killedBy: '',	name: '',	takenBy: ''})
 	}
 
 	handleChange = event => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
+		if (!!this.state.errors[event.target.name]) {
+			let errors = Object.assign({}, this.state.errors);
+			delete errors[event.target.name];
+			this.setState({
+				[event.target.name]: event.target.value,
+				errors
+			});
+		} else {
+			this.setState({ [event.target.name]: event.target.value});
+		}
 	}
 
 	render () {
-		const { causeOfDeath, description, killedBy, name, takenBy } = this.state;
-
+		const { causeOfDeath, description, killedBy, name, takenBy, errors } = this.state;
+		
 		return (
 			<div className='add-char-wrapper'>
 				<div className='add-char__header'>Add New Character</div>
@@ -35,34 +55,39 @@ export default class AddCharacter extends Component {
 					<input
 						onChange={ this.handleChange }
 						type='text' name='name'
-						placeholder='name'
+						className = { errors.name ? 'add_char__input--error' : '' }
+						placeholder={errors.name ? errors.name : 'name' }
 						value={ name }
 					/><br />
 					<input
 						onChange={ this.handleChange }
 						type='text' name='description'
 						value={ description }
-						placeholder='description'
+						className = { errors.description ? 'add_char__input--error' : '' }
+						placeholder={errors.description ? errors.description : 'description' }
 					/><br />
 					<input
 						onChange={ this.handleChange }
 						type='text' name='killedBy'
 						value={ killedBy }
-						placeholder='killed by'
+						className = { errors.killedBy ? 'add_char__input--error' : '' }
+						placeholder={errors.killedBy ? errors.killedBy : 'killed by' }
 					/><br />
 					<input
 						onChange={ this.handleChange }
 						type='text' name='takenBy'
 						value={ takenBy }
-						placeholder='taken by'
+						className = { errors.takenBy ? 'add_char__input--error' : '' }
+						placeholder={errors.takenBy ? errors.takenBy : 'taken by' }
 					/><br />
 					<input
 						onChange={ this.handleChange }
 						type='text' name='causeOfDeath'
 						value={ causeOfDeath }
-						placeholder='cause of death'
+						className = { errors.causeOfDeath ? 'add_char__input--error' : '' }
+						placeholder={errors.causeOfDeath ? errors.causeOfDeath : 'cause of death' }
 					/><br />
-					<button className='add_char__button--flat' disabled={ 0 }>Add</button>
+					<button className='add_char__button--flat'>Add</button>
 				</form>
 			</div>
 		);
